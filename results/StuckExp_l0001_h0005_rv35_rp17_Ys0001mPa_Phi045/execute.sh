eeb306e86ffbe6f591536b39346a9fd60497f3f6
Masahiro Kondo <kondo.masahiro@aist.go.jp>
2023-12-08 05:07:41 +0900

    delete StuckExp case

--- results/StuckExp_l0001_h0005_rv35_rp17_Ys0001mPa_Phi045/execute.sh
+++
@@ -1,3 +0,0 @@
-../../source/MphImplicit stuck.data stuck.grid stuck%03d.prof stuck%03d.vtk stuck.log 1
-
-
--- results/StuckExp_l0001_h0005_rv35_rp17_Ys0001mPa_Phi045/execute_gcc4.sh
+++
@@ -1,3 +0,0 @@
-../../source/Mph_gcc stuck.data stuck.grid stuck%03d.prof stuck%03d.vtk stuck_gcc4.log 4
-
-
